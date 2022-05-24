fb0dd51502a0875c2936dc7ce8275c854fb51fae
Koski Jenna <s2200123@BC-C02GR0BPQ6LR-MacBook-Air.local>
2022-05-24 16:16:52 +0300

    hero img added, welcome text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import image from "../assets/images/engin-akyurt-3ORoQEJY9LA-unsplash.jpg";
 
 const Home = () => {
-  return <div>Homepage will be here</div>;
+  return (
+    <div>
+      <img className="hero_img" src={image} alt="glass of beer" />
+      <div>
+        <h1>Welcome to Error Squirrel Beer Shop!</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
