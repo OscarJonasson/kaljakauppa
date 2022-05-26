704f4951dfafc6a3ceca9a17b28f2670889af47d
Jonasson Oscar <oscjjonasson@gmail.com>
2022-05-26 15:23:03 +0300

    contact styling

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -3,7 +3,7 @@ const ContactUs = () => {
   return (
     <div className={classes.container}>
       <form className={classes.form}>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Your email</label>
         <input type="email" id="email"></input>
         <label htmlFor="message">Message</label>
         <textarea id="message" cols={50}></textarea>
