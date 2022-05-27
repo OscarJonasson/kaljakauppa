8d9f23adec94210a3ccbd67e57e002280a766d63
MikkeBee <mjblakley87@gmail.com>
2022-05-27 16:28:56 +0300

    join and newsletter pages made, minor style changes to other elements

---
+++ src/components/Newsletter/Newsletter.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./Newsletter.module.css";
+
+const Newsletter = () => {
+  return (
+    <section className={classes.newsletterSection}>
+      <h2 className={classes.newsletterHeader}>Join our newsletter!</h2>
+      <p className={classes.newsletterText}>
+        Get the latest news on new products, specials, giveaways, and more! We
+        even give our newsletter subscribers access to special deals before
+        anyone else. So what are you waiting for? Sign up now!
+      </p>
+      <form className={classes.newsletterForm}>
+        <div>
+          <label className={classes.emailLabel} htmlFor="email">
+            Email
+          </label>
+          <input className={classes.emailInput} type="email" id="email"></input>
+        </div>
+        <div>
+          <button className={classes.sendButton} onSubmit={""}>
+            Sign me up!
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Newsletter;
