cdbde9eb7a3b275569f4e7b64465b05a2c670652
MikkeBee <mjblakley87@gmail.com>
2022-05-29 14:58:23 +0300

    removed hamburger addon and reverted all nav links back to previous states

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { slide as Menu } from "react-burger-menu";
 
 const Nav = () => {
   return (
-    // <nav>
-    //   <ul>
-    //     <li>
-    <Menu>
-      <Link to="/">Home</Link>
-      <Link to="/beers">Beers</Link>
-      <Link to="/aboutUs">About Us</Link>
-      <Link to="/contact">Contact Us</Link>
-      <Link to="/joinus">Join Us</Link>
-      <Link to="/newsletter">Newsletter</Link>
-    </Menu>
-    //     </li>
-    //   </ul>
-    // </nav>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/beers">Beers</Link>
+          <Link to="/aboutUs">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/joinus">Join Us</Link>
+          <Link to="/newsletter">Newsletter</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
