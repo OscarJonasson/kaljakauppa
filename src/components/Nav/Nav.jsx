d89a30dc884c83f9839f2ce00a8c00b1acb2a52f
MikkeBee <mjblakley87@gmail.com>
2022-05-29 16:27:39 +0300

    updated burger menu styling, now at simple mvp version

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import classes from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/beers">Beers</Link>
-          <Link to="/aboutUs">About Us</Link>
-          <Link to="/contact">Contact Us</Link>
-          <Link to="/joinus">Join Us</Link>
-          <Link to="/newsletter">Newsletter</Link>
+    <nav className={classes.navNav}>
+      <ul className={classes.ulLink}>
+        <li className={classes.liLink}>
+          <Link to="/" className={classes.navLink}>
+            Home
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/beers" className={classes.navLink}>
+            Beers
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/aboutUs" className={classes.navLink}>
+            About Us
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/contact" className={classes.navLink}>
+            Contact Us
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/joinus" className={classes.navLink}>
+            Join Us
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/newsletter" className={classes.navLink}>
+            Newsletter
+          </Link>
         </li>
       </ul>
     </nav>
