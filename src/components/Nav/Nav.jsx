24f1bbcacbe9f376c85a7f5657cfc4749268aea8
Jonasson Oscar <oscjjonasson@gmail.com>
2022-05-30 18:17:36 +0300

    nav fluidity

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -17,42 +17,40 @@ const Nav = props => {
   // console.log(props.IsMenuOpen);
 
   return (
-    <div className={classes.maybe}>
-      <nav className={classes.navNav}>
-        <ul className={classes.ulLink}>
-          <li className={classes.liLink}>
-            <Link to="/" className={classes.navLink}>
-              Home
-            </Link>
-          </li>
-          <li className={classes.liLink}>
-            <Link to="/beers" className={classes.navLink}>
-              Beers
-            </Link>
-          </li>
-          <li className={classes.liLink}>
-            <Link to="/aboutUs" className={classes.navLink}>
-              About Us
-            </Link>
-          </li>
-          <li className={classes.liLink}>
-            <Link to="/contact" className={classes.navLink}>
-              Contact Us
-            </Link>
-          </li>
-          <li className={classes.liLink}>
-            <Link to="/joinus" className={classes.navLink}>
-              Join Us
-            </Link>
-          </li>
-          <li className={classes.liLink}>
-            <Link to="/newsletter" className={classes.navLink}>
-              Newsletter
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={classes.navNav}>
+      <ul className={classes.ulLink}>
+        <li className={classes.liLink}>
+          <Link to="/" className={classes.navLink}>
+            Home
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/beers" className={classes.navLink}>
+            Beers
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/aboutUs" className={classes.navLink}>
+            About Us
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/contact" className={classes.navLink}>
+            Contact Us
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/joinus" className={classes.navLink}>
+            Join Us
+          </Link>
+        </li>
+        <li className={classes.liLink}>
+          <Link to="/newsletter" className={classes.navLink}>
+            Newsletter
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
