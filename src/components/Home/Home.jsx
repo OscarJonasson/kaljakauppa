7e78bdd16f077ea2c63382933f23466a932ae324
Jonasson Oscar <oscjjonasson@gmail.com>
2022-05-31 11:55:55 +0300

    a dded answers to verify  button presses, conditional rendering for it on homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,25 +4,32 @@ import classes from "./Home.module.css";
 import axios from "axios";
 import CardHome from "../CardHome/CardHome";
 import image from "../../assets/images/missy-fant-OMIgwm1i_NY-unsplash.jpg";
+import VerifyAge from "../VerifyAge/VerifyAge";
 
 const Home = () => {
   const [beers, setBeers] = useState([]);
+  // Set this to false if you want to test the modal window
+  const [ageCheck, setAgeCheck] = useState(true);
+
+  const checkAge = () => {
+    setAgeCheck(true);
+  };
 
   useEffect(() => {
     axios
       // .get("http://localhost:3011/beers")
       .get("https://kaljakauppa-json.herokuapp.com/beers")
-      .then((res) => setBeers(res.data))
-      .catch((err) => console.log(err));
+      .then(res => setBeers(res.data))
+      .catch(err => console.log(err));
   }, []);
 
   const selection = [6, 2, 8];
   const special = (beers, selection) => {
     return beers
-      .filter((beer) => {
+      .filter(beer => {
         return selection.includes(beer.id);
       })
-      .map((beer) => {
+      .map(beer => {
         return (
           <Link to={`beers/${beer.id}`}>
             <CardHome key={beer.id} beer={beer} />
@@ -30,8 +37,10 @@ const Home = () => {
         );
       });
   };
+
   return (
     <div>
+      {!ageCheck && <VerifyAge check={checkAge} />}
       <div className={classes.hero_container}>
         <img className={classes.hero_img} src={image} alt="glass of beer" />
         <h1 className={classes.hero_heading}>
