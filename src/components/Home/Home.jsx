47ddc6123adc4ee2a3474328f21721e17596a236
MikkeBee <mjblakley87@gmail.com>
2022-06-03 16:50:57 +0300

    remove faulty components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,6 @@ import axios from "axios";
 import CardHome from "../CardHome/CardHome";
 import image from "../../assets/images/alban-martel-nhX8QhXMBkM-unsplash.jpg";
 import VerifyAge from "../VerifyAge/VerifyAge";
-import CoverflowEffect from "../Swiper/CoverflowEffect";
 
 const Home = () => {
   const [beers, setBeers] = useState([]);
@@ -56,9 +55,6 @@ const Home = () => {
       </div>
       <section className={classes.specials}>
         {special(beers, selection)}
-      </section>
-      <section>
-        <CoverflowEffect />
       </section>
     </div>
   );
