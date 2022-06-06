c3ea7d4a3e0f3d0641b19fdca3d6806fb3f93d67
Koski Jenna <s2200123@BC-C02GR0BPQ6LR-MacBook-Air.local>
2022-06-06 12:17:17 +0300

    Account page started

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import classes from "./Account.module.css";
 
 const Account = () => {
-  return <div>Account page here</div>;
+  return (
+    <div>
+      <form>
+        <h3>Sign Up</h3>
+        <div className={classes.nameArea}>
+          <input
+            type="text"
+            name="firstname"
+            className={classes.doubleInput1}
+            placeholder="First Name"
+            required
+          />
+          <input
+            type="text"
+            name="lastname"
+            className={classes.doubleInput2}
+            placeholder="Last Name"
+            required
+          />
+        </div>
+        <input
+          type="text"
+          name="address"
+          className={classes.singleInput}
+          placeholder="Address"
+          required
+        />
+        <input
+          type="text"
+          name="extraaddress"
+          className={classes.singleInput}
+          placeholder="Apartment, suite, etc."
+          required
+        />
+        <input
+          type="text"
+          name="city"
+          className={classes.singleInput}
+          placeholder="City"
+          required
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Account;
