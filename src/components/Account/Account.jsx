a0b9233b32c1aabad0fcda6426483e14fb8f6e3c
Koski Jenna <s2200123@BC-C02GR0BPQ6LR-MacBook-Air.local>
2022-06-06 14:48:28 +0300

    Account base done, no stylings

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,10 +1,21 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import classes from "./Account.module.css";
 
 const Account = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v2/all").then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <form>
+      <form className={classes.accountForm}>
         <h3>Sign Up</h3>
         <div className={classes.nameArea}>
           <input
@@ -22,6 +33,13 @@ const Account = () => {
             required
           />
         </div>
+        <input
+          type="email"
+          name="email"
+          className={classes.singleInput}
+          placeholder="Email address"
+          required
+        />
         <input
           type="text"
           name="address"
@@ -43,6 +61,35 @@ const Account = () => {
           placeholder="City"
           required
         />
+        <div>
+          <select
+            typeof="text"
+            name="country"
+            className={classes.countrySelect}
+            required
+          >
+            <option selected disabled hidden>
+              Country
+            </option>
+            {countries.map((country) => (
+              <option key={country.name}>{country.name}</option>
+            ))}
+          </select>
+          <input
+            type="text"
+            name="postcode"
+            className={classes.doubleInput2}
+            placeholder="Postal Code"
+            required
+          />
+        </div>
+        <input
+          type="tel"
+          name="phone"
+          className={classes.singleInput}
+          placeholder="Phone"
+        />
+        <button className={classes.signup_btn}>Sign Up</button>
       </form>
     </div>
   );
